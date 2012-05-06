e40fd4c3f95a4124df7dc16ab15d4f7d76f573ae
free coder <iamon3.free@gmail.com>
2012-05-06 17:16:33 +0530

    Simple thread synchronization with boolean with Pros and cons

---
+++ AdvancedConcepts/src/com/threads/caveOfProgramming/synchronization/withBoolean/App.java
@@ -0,0 +1,52 @@
+/**
+ * This programme is developed by caveofprogramming
+ * Can be found at http://www.caveofprogramming.com/
+ * 
+ */
+package com.threads.caveOfProgramming.synchronization.withBoolean;
+import java.util.Scanner;
+
+
+class Processor extends Thread{
+
+	// volatile is important
+	// Otherwise theoretically its possible that 
+	// running value is cached by this thread so it always true
+	// so thread will keep on running
+	private volatile boolean running = true;
+
+	@Override
+	public void run() {
+
+		while(running){
+			System.out.println("Running ");
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Stopped ");
+	}
+
+	public void shutdown(){
+		running = false;
+	}
+
+}
+
+public class App {
+
+	public static void main(String[] args) {
+	
+	  // Instanciate Thread
+      Processor processor = new Processor();
+      
+      //Start Thraed
+      processor.start();
+      
+      //To stop running thread, Press "Return" Key.
+      new Scanner(System.in).nextLine();
+      processor.shutdown();
+	}
+}
