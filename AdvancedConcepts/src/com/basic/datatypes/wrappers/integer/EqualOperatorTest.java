06e19cd1ea2753b86e758f311fc39a8bcc0f7958
free coder <iamon3.free@gmail.com>
2014-07-12 18:51:42 +0530

    Added more examples

--- AdvancedConcepts/src/com/basic/datatypes/wrappers/integer/EqualOperatorTest.java
+++ AdvancedConcepts/src/com/basic/datatypes/wrappers/integer/EqualOperatorTest.java
@@ -1,7 +1,5 @@
 package com.basic.datatypes.wrappers.integer;
 
-import java.util.HashMap;
-
 public class EqualOperatorTest {
 
 	public EqualOperatorTest(){
@@ -13,13 +11,14 @@ public class EqualOperatorTest {
 		Integer b = 5;
 		Integer c = new Integer(5);
 		
-		System.out.println("a == b : " + (a == b));
+		System.out.println("a == b : " + (a == b)); // true
 		b +=1;
-		System.out.println("After incrementing 'b' a == b : " + (a == b));
+		System.out.println("After incrementing 'b' a == b : " + (a == b)); //false
 
-		System.out.println("a == c : " + (a == c));
+		System.out.println("a == c : " + (a == c)); // false
+		System.out.println("a.equals(c) : " + a.equals(c)); // true
 		c += 1;
-		System.out.println("c : " + c);
+		System.out.println("c : " + c); // 6
 		
 	}
 }
---
+++ AdvancedConcepts/src/com/collections/map/hashMap/withoutEquals/TestHashMapWithoutEquals.java
@@ -0,0 +1,56 @@
+package com.collections.map.hashMap.withoutEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestHashMapWithoutEquals {
+
+	public static void main(String[] args) {
+		
+		HashMap<Employee, Employee> emps = new HashMap<Employee, Employee>();
+		Employee e1 = new Employee("1", "Arun");
+		Employee e2 = new Employee("2", "Varun");
+		Employee e3 = new Employee("3", "Tarun");
+		Employee e4 = new Employee("2", "Karun");
+		emps.put(e1,e1);
+		emps.put(e2,e2);
+		emps.put(e3,e3 );
+		System.out.println("Before Size : " + emps.size() +", elements : " + emps);
+		
+		emps.put(e4,e4);	
+		System.out.println("After Size : " + emps.size() +", elements : " + emps);
+		
+		System.out.println("E4 : " + emps.get(e4));
+		
+	}
+}
+
+class Employee{
+	String deptId;
+	String name;
+	
+	public Employee(String deptId, String name){
+		this.deptId = deptId;
+		this.name = name;
+	}
+	
+	@Override
+	public int hashCode(){
+		return Integer.parseInt(this.deptId);
+	}
+	
+	@Override	
+	public boolean equals(Object o){
+		Employee e = (Employee) o;
+		  return this.name.equals(e.name);	
+	}
+	
+	@Override	
+	public String toString(){
+		return this.deptId +" : " + this.name;
+	}
+}
+
+class Person{
+	
+}
