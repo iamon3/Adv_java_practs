394305eba233176198832e48fe50286610dda62b
free coder <iamon3.free@gmail.com>
2014-08-13 11:35:15 +0530

    Added the comments

--- AdvancedConcepts/src/com/ds/patricia/streamsearch/main/Main.java
+++ AdvancedConcepts/src/com/ds/patricia/streamsearch/main/Main.java
@@ -4,6 +4,24 @@ import com.ds.patricia.streamsearch.impl.DictionaryPatriciaImpl;
 import com.ds.patricia.streamsearch.impl.FileFormatCSV;
 import com.ds.patricia.streamsearch.impl.InputSourceCharArray;
 
+/**
+ * Problem Statement :
+ * You will be receiving a Live InputStream and from where characters will be flowing in. 
+ * You will receive a list of words from a file which is comma separated. 
+ * From a live stream you will have to find out when you encountered particular word that you have received in an input file.
+ * 
+ * Assume Input Data as: helloiaminpunewhereiwillbewatchingamovieiwillalsoliketohavealunchafterwatchingamovie
+ * Words to search: will like chaf
+ * 
+ * Evaluation:
+ * 1) Try and implement in a single pass.
+ * 
+ * Description :
+ * The solution developed by me uses the dictionary which is backed by the particia data structure. 
+ * Although, I am not doing the search in a single pass, solution is still bit efficient in terms of time and memory complexity.
+ * Another solution for the same problem can be found at : https://github.com/yuvitag/DirectorySearch
+
+ */
 public class Main {
 	public static void main(String[] args) {
 		
